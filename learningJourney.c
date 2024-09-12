ba8ff4f13cf6e9e223f693ce18a2251adcba0ffa
Hashmary <93223957+Hashmary@users.noreply.github.com>
2024-09-12 13:48:27 +0200

    Add files via upload

--- learningJourney.c
+++ learningJourney.c
@@ -1,18 +1,80 @@
-// TODO: header nachtragen
-// Totally quick & dirty!!!
-// Build: gcc -W -pedantic -o lJ learningJourney.c
+/****************************************************************************
+ * @file:   learningJourney.c
+ * @author: Marion Bohr (C)
+ * @date:   2024-08-21
+ * @build:  gcc -W -pedantic -o lJ learningJourney.c
+ * @description:
+ *    Totally quick & dirty!!!
+ *    Start with the column in domainsConcepts containing the highest
+ *    familiarity value in persona.
+ *    Find the max familiarity in this column. Save the corresponding line
+ *    number. This line number gives us the next certificate to work for.
+ *    Each certificate increases familiarity.
+ *    We continue finding the next maximum familiarity in the domainsConcepts
+ *    - excluding the line with the certificate the columns with the already
+ *    known maximum familiarity.
+ *    In the end, we get a sequence of certificates and thus the learning path.
+ ****************************************************************************/
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {
-    // Vordefinierte 2x7 Tabelle for persona with familiarities
-    float persona[2][7] = {
+#define MAX_DOMAINS   7
+#define MAX_CONCEPTS  7
+#define MAX_PERSONAS  2
+
+bool isInArray(int element, int array[], int size);
+
+void maxFamiliarityPerColumn(const float domainsConcepts[MAX_DOMAINS][MAX_CONCEPTS],
+  int certificates[MAX_DOMAINS], float* maxFamiliarity, int searchColumn,
+  int searchLine, int* certificateLine);
+
+// Check whether certificate is already suggested
+bool isInArray(int element, int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    //printf("---- %d %d\n", element, array[i]);
+    if (array[i] == element) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Find maximum familiarity per column/ per concept
+void maxFamiliarityPerColumn(const float domainsConcepts[MAX_DOMAINS][MAX_CONCEPTS],
+  int certificates[MAX_DOMAINS], float* maxFamiliarity, int searchColumn,
+  int searchLine, int* certificateLine) {
+
+  *maxFamiliarity = 0.0f;
+  *certificateLine = 0;
+  int j = searchColumn;
+
+  for (int i = 0; (i < MAX_DOMAINS); i++) {
+    //printf("##### %d\n", i);
+    // +1 display
+
+    // Only for conepts which are not yet suggested within a certificate
+    if (!(isInArray((i + 1), certificates, MAX_DOMAINS))){
+      if (domainsConcepts[i][j] > *maxFamiliarity) {
+        *maxFamiliarity = domainsConcepts[i][j];
+        *certificateLine = i;
+      }
+    }
+  }
+}
+
+//---------------------------------------------------------------------------
+
+int main(void) {
+
+    // Predifined 2x7 test table for persona with familiarities
+    float persona[MAX_PERSONAS][MAX_CONCEPTS] = {
         { 0.2, 0.5, 0.3, 0.4, 0.6, 0.3, 0.1 },  // 0 = Emily
         { 0.4, 0.5, 0.6, 0.3, 10.2, 0.4, 0.2 }  // 1 = Jochen
     };
 
-    // Vordefinierte 7x7 Tabelle for familiarities in domains and concepts
-    float domainsConcepts[7][7] = {
+    // Predifined 7x7 test table for familiarities in domains and concepts
+    float domainsConcepts[MAX_DOMAINS][MAX_CONCEPTS] = {
         { 0.9, 0.6, 0.4, 0.2, 0.3, 0.5, 0.9 },
         { 0.9, 0.7, 0.3, 0.3, 0.4, 0.5, 0.8 },
         { 0.7, 0.8, 0.5, 0.4, 0.3, 0.6, 0.7 },
@@ -22,68 +84,133 @@ int main() {
         { 0.9, 0.5, 0.9, 0.6, 0.5, 0.6, 0.8 }
     };
 
-    // reached certificates
-    int certificates[7] ={ 0, 0, 0, 0, 0, 0, 0 };
+    // Planned certificates
+    int certificates[MAX_DOMAINS] = { 0, 0, 0, 0, 0, 0, 0 };
+    int certificateIndex = 0;
+    int certificateLine = 0;
 
-        // Ausgabe der Tabelle
-    printf("\n\n7x7 vordefinierte Tabelle:\n\n");
-    for (int i = 0; i < 7; i++) {
-        for (int j = 0; j < 7; j++) {
+    // Output test table domainsConcepts
+    printf("\n\n7x7 predefined test table domainsConcepts:\n\n");
+    for (int i = 0; i < MAX_DOMAINS; i++) {
+        for (int j = 0; j < MAX_CONCEPTS; j++) {
             printf("%3.1f ", domainsConcepts[i][j]);
         }
         printf("\n");
     }
 
-    // Bestimme in der Persona Tabelle In der Zeile für Emily, die maximale Familiarity und ihre Position
-    // Variablen für Maximalwert und Spaltennummer
+    // Determine via test table persona in the line for Emily the maximum
+    // familiarity and the position of the column.
     float maxFamiliarity = persona[0][0];
-    int maxColumn = 0;
+    int startColumn = 0;
 
-    // Bestimme Maximalwert und Spaltennummer in der ersten Zeile
-    // int searchLine = 0; // im Test steht hier Emily
-    for (int j = 0; (j < 7); j++) {
+    // Determine start values for e.g. Emily
+    // int searchLine = 0; // Emily
+    for (int j = 0; (j < MAX_CONCEPTS); j++) {
         if (persona[0][j] > maxFamiliarity) {
             maxFamiliarity = persona[0][j];
-            maxColumn = j;
+            startColumn = j;
         }
     }
 
-        // Ausgabe der Tabelle
+    // Output test table persona
     printf("\n\n2x7 vordefinierte Tabelle:\n\n");
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 7; j++) {
+    for (int i = 0; i < MAX_PERSONAS; i++) {
+        for (int j = 0; j < MAX_CONCEPTS; j++) {
             printf("%3.1f ", persona[i][j]);
         }
         printf("\n");
     }
 
-    // Ausgabe des Maximalwerts und der Spaltennummer
-    printf("\nEmily's maximal familiarity : %3.1f\n", maxFamiliarity);
-    printf("Column: %d\n", maxColumn + 1); // +1 für 1-basierte Nummerierung
+    printf("\nEmily's maximum familiarity : %3.1f\n", maxFamiliarity);
+    printf("Column: %d\n", startColumn + 1); // +1 für 1-basierte Nummerierung
 
-    // Finde in der Tabelle DomainConcept in der entsprechenden Spalte die maximale Familiarity
-    // Find in DomainsConcepts next familiarity beginning with starting familiarity column
-    /*for (int i = 0; i < 7; i++) {
-            for (j = 0; j < 7; j++) {
-            }
-    }
-    int k = maxColumn;
-    maxFamiliarity = 0;
-    for (int i = k; (i < 7); i++) {
-        if (persona[i][k] > maxFamiliarity) {
-            maxFamiliarity = persona[0][j];
-            maxColumn = j;
+    // Find in domainsConcepts next familiarity beginning with starting familiarity column
+    float familiarity = 0.0f;
+
+    int startLine = 0;
+
+    maxFamiliarityPerColumn(domainsConcepts, certificates, &familiarity,
+      startColumn, startLine, &certificateLine);
+    printf("\nEmily's most familiar concept to start with: %3.1f %d\n",
+      familiarity, certificateLine + 1);
+    certificates[certificateIndex] = certificateLine + 1;
+    certificateIndex++;
+    //printf("Nr/ Certificate: %d %d\n", certificateIndex, certificates[certificateIndex]); // +1 für 1-basierte Nummerierung
+
+    for (int j = 0; j < MAX_CONCEPTS; j++) {
+      for (int i = 0; i < MAX_DOMAINS; i++) {
+        if (certificateIndex < MAX_DOMAINS) {
+          maxFamiliarityPerColumn(domainsConcepts, certificates, &familiarity,
+            j, i, &certificateLine);
+          printf("\nEmily's next domain: %3.1f %d\n", familiarity, certificateLine + 1);
+          certificates[certificateIndex] = certificateLine + 1;
+          certificateIndex++;
+          //printf("Nr/ Certificate: %d %d\n", certificateIndex, certificates[certificateIndex]); // +1 für 1-basierte Nummerierung
         }
-    } */
+      }
+    }
 
-    // Start with the column in domainConcepts containing showing the highest familiarity value in persona.
-    // Find the max familirity in this column. Save the corresponding line number. This line number gives us
-    // the next certificate to work for. The certificate increases familiarity.
-    // We continue finding the next max familiarity in the domainsConcepts - excludingly the line with the
-    // certificate.
-    // We exclude always the lines in domainsConcepts which have a certificate (entry) and the columns with the
-    // max familiarity.
-    // In the end, we have a seqence of certificates and thus the learning path.
+    for (int i = 0; i < MAX_DOMAINS; i++) {
+      printf("Certificate %d: %d\n", i + 1, certificates[i]);
+    }
+
+    //---------------------------------------------------------------------------
+
+    for (int i = 0; i < MAX_DOMAINS; i++) {
+      certificates[i] = 0;
+    }
+    certificateIndex = 0;
+    certificateLine = 0;
+
+    maxFamiliarity = 0;
+    startColumn = 0;
+
+    // int searchLine = 1; // Jochen
+    for (int j = 0; (j < MAX_CONCEPTS); j++) {
+        if (persona[1][j] > maxFamiliarity) {
+            maxFamiliarity = persona[1][j];
+            startColumn = j;
+        }
+    }
+
+    printf("\nJochen's maximum familiarity : %3.1f\n", maxFamiliarity);
+    printf("Column: %d\n", startColumn + 1); // +1 für 1-basierte Nummerierung
+
+    // Find in domainsConcepts next familiarity beginning with starting familiarity column
+    familiarity = 0.0f;
+
+    startLine = 0;
+
+    maxFamiliarityPerColumn(domainsConcepts, certificates, &familiarity,
+      startColumn, startLine, &certificateLine);
+    printf("\nJochen's most familiar concept to start with: %3.1f %d\n",
+      familiarity, certificateLine + 1);
+    certificates[certificateIndex] = certificateLine + 1;
+    certificateIndex++;
+    //printf("Nr/ Certificate: %d %d\n", certificateIndex, certificates[certificateIndex]); // +1 für 1-basierte Nummerierung
+
+    for (int j = 0; j < MAX_CONCEPTS; j++) {
+      for (int i = 0; i < MAX_DOMAINS; i++) {
+        if (certificateIndex < MAX_DOMAINS) {
+          maxFamiliarityPerColumn(domainsConcepts, certificates, &familiarity,
+            j, i, &certificateLine);
+          printf("\nJochen's next domain: %3.1f %d\n", familiarity, certificateLine + 1);
+          certificates[certificateIndex] = certificateLine + 1;
+          certificateIndex++;
+          //printf("Nr/ Certificate: %d %d\n", certificateIndex, certificates[certificateIndex]); // +1 für 1-basierte Nummerierung
+        }
+      }
+    }
+
+    for (int i = 0; i < MAX_DOMAINS; i++) {
+      printf("Certificate %d: %d\n", i + 1, certificates[i]);
+    }
+
     return 0;
-}
 
+    /* TODO:
+      - I/O functions, reading tables from input file (csv)
+      - more flexible logic
+      - more elaborated logic
+    */
+}
